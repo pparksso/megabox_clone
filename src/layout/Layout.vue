e3d060fed93f03c096bec063104c63282dd0d48f
Park Sohee <thgml5143@hanmail.net>
2022-11-17 11:40:53 +0900

    chore: 프로젝트 구조 변경, firebase

--- src/layout/Layout.vue
+++ src/layout/Layout.vue
@@ -1,5 +1,10 @@
 <template>
+  <Header />
   <router-view></router-view>
+  <Footer />
 </template>
-<script setup></script>
+<script setup>
+import Header from "../components/common/Header.vue";
+import Footer from "../components/common/Footer.vue";
+</script>
 <style lang="scss" scoped></style>
